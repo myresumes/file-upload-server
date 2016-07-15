a36d4eb8e7918caf0f2c91d057ee7d1321b3b747
prateek <prateek.pronoc@gmail.com>
2016-07-15 22:21:52 +0530

    basic get by id , get-all and post verbose implemented

---
+++ src/index.js
@@ -0,0 +1,49 @@
+var restify = require("restify");
+var Promise = require('bluebird');
+var server = restify.createServer({
+    name: 'myapp',
+    version: '1.0.0'
+});
+// server.use(function crossOrigin(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     return next();
+// });
+
+server.use(restify.CORS({
+
+  // Defaults to ['*'].
+  origins: ['http://localhost:8081'], 
+
+  // Defaults to false.
+  credentials: true,
+
+  // Sets expose-headers.
+  headers: ['x-foo']   
+
+}));
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
+    //return resolve(server);
+});
+
+
+server.post('/api/upload-file', function create(req, res, next) {
+    res.send(201, req.body);
+    return next();
+});
+
+
+server.get('/api/test-api/:id', function create(req, res, next) {
+    res.send(201, req.params);
+    return next();
+});
+
+server.get('/api/test-get-all-api', function create(req, res, next) {
+    res.send(201, req.params.name);
+    return next();
+});
